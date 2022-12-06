fa402a589f6939f6272eb9e5b5c9c3c49c83dfb9
yensan0512 <yensan1254@gmail.com>
2022-12-06 10:28:12 +0800

    'first'

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header(){
+    return(
+    <div className="header">
+        <div className="header_logo">
+        <StorefrontIcon className="header_logoImage" fontSize="large"/>
+        <h2 className="header_logoTitle">eShop</h2>
+        </div>
+
+        <div className="header_search">
+            <input type="text" className="header_searchInput" />
+            <SearchIcon className="header_searchIcon"/>
+        </div>
+
+        <div className="header_nav">
+            <div className="nav_item">
+                <span className="nav_itemLineOne">Hello Guest</span>
+                <span className="nav_itemLineTwo">Sign In</span>
+            </div>
+
+            <div className="nav_item">
+                <span className="nav_itemLineOne">Your</span>
+                <span className="nav_itemLineTwo">Shop</span>
+            </div>
+
+            <div className="nav_item">
+                <ShoppingBasketIcon className="itemBasket" fontSize="large"/>
+                <span className="nav_itemLineTwo">0</span>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Header
